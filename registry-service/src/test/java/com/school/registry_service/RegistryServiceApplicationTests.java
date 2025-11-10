2636f6ef796a009a6f99e28aec68749dbd31df26
yvanna Djaha <yvannadjaha@gmail.com>
2025-11-10 14:56:24 +0100

    initialisation

---
+++ registry-service/src/test/java/com/school/registry_service/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
